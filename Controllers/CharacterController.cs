d773b0f89555d598b358deba72043db02927615b
Patrick <patrick@godknows.de>
2022-10-25 11:45:31 +0200

    Character Service

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -14,24 +14,30 @@ namespace dotnet7rpg_prep.Controllers
             new Character(),
             new Character {Id = 1, Name = "Sam" }
         };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
