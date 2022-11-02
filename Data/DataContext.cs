d1e1a33fb39de5ce8db9e69d4d8ade2db8a5de52
Patrick <patrick@godknows.de>
2022-11-02 13:44:27 +0100

    Many-To-Many Relationship with Skills

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -15,5 +15,6 @@ namespace dotnet7rpg_prep.Data
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Weapon> Weapons => Set<Weapon>();
+        public DbSet<Skill> Skills => Set<Skill>();
     }
 }
--- Models/Character.cs
+++ Models/Character.cs
@@ -16,5 +16,6 @@ namespace dotnet7rpg_prep.Models
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public User? User { get; set; }
         public Weapon? Weapon { get; set; }
+        public List<Skill>? Skills { get; set; }
     }
 }
