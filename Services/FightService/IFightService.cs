60ecb855201ea8a79173104a1aa3191d9666b2f9
Patrick <patrick@godknows.de>
2022-11-04 20:44:19 +0100

    Attack with Weapons

--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet7rpg_prep.Dtos.Fight;
 
 namespace dotnet7rpg_prep.Services.FightService
 {
     public interface IFightService
     {
-        
+        Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
     }
 }
